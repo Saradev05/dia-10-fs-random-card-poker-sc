f0bb397794c7f3b0f8fbdee0b8f420ffd74b1076
Saradev05 <devyanand@gmail.com>
2021-03-19 18:44:18 +0000

    falta iconos

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// Poker cards generator
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var suits = ["diamonds", "hearts", "shamrocks", "spades"];
+  var numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let newSuit = Math.floor(Math.random() * suits.length);
+  let palo = document.querySelector(".suits");
+  palo.innerHTML = suits[newSuit];
+
+  let paloInferior = document.querySelector(".bottomSuit");
+  paloInferior.innerHTML = suits[newSuit];
+
+  let newNumber = Math.floor(Math.random() * numbers.length);
+  let value = document.querySelector(".number");
+  value.innerHTML = numbers[newNumber];
 };
