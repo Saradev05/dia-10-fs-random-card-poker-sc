e64d62993d5bd4e057cc11afd623dfe867aeebf3
Saradev05 <devyanand@gmail.com>
2021-03-29 15:58:57 +0000

    en rojo

--- src/app.js
+++ src/app.js
@@ -4,12 +4,15 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
 
 // Poker cards generator
-
 window.onload = function() {
   //write your code here
-  var suits = ["\u2666", "\u2665", "\u2663", "\u2660"];
+  //   var suitsContent = ["\u2666", "\u2665", "\u2663", "\u2660"];
+  var suitsContent = ["\u2665", "\u2660", "\u2663", "\u2666"];
+  var suitsClasses = ["corazon", "espadas", "trebol", "diamante"];
   var numbers = [
     "A",
     "2",
@@ -26,14 +29,51 @@ window.onload = function() {
     "K"
   ];
 
-  let newSuit = Math.floor(Math.random() * suits.length);
-  let palo = document.querySelector(".suits");
-  palo.innerHTML = suits[newSuit];
-
+  let randomPosition = Math.floor(Math.random() * suitsClasses.length);
+  let paloTop = document.querySelector(".topSuit");
   let paloInferior = document.querySelector(".bottomSuit");
-  paloInferior.innerHTML = suits[newSuit];
+  paloTop.innerHTML = suitsContent[randomPosition];
+  paloInferior.innerHTML = suitsContent[randomPosition];
+
+  paloTop.className = "topSuit";
+  paloTop.classList.add(suitsClasses[randomPosition]);
+  paloInferior.className = "suits bottomSuit d-flex flex-item";
+  paloInferior.classList.add(suitsClasses[randomPosition]);
 
   let newNumber = Math.floor(Math.random() * numbers.length);
   let value = document.querySelector(".number");
   value.innerHTML = numbers[newNumber];
 };
+
+// // Poker cards generator
+
+// window.onload = function() {
+//   //write your code here
+//   var suits = ["\u2666", "\u2665", "\u2663", "\u2660"];
+//   var numbers = [
+//     "A",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//     "10",
+//     "J",
+//     "Q",
+//     "K"
+//   ];
+
+//   let newSuit = Math.floor(Math.random() * suits.length);
+//   let palo = document.querySelector(".suits");
+//   palo.innerHTML = suits[newSuit];
+
+//   let paloInferior = document.querySelector(".bottomSuit");
+//   paloInferior.innerHTML = suits[newSuit];
+
+//   let newNumber = Math.floor(Math.random() * numbers.length);
+//   let value = document.querySelector(".number");
+//   value.innerHTML = numbers[newNumber];
+// };
